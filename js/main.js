d56377bf7316f1f913fb80e40a6148d634306dcf
Carmen20 <carmenrosa.araceli.arias.arias@gmail.com>
2016-05-18 12:22:30 -0500

    faces-90%

--- js/main.js
+++ js/main.js
@@ -1,19 +1,50 @@
-
 $(document).ready(function(){
+    var coder,coderm;
   $('#sede').change(function(){
   var value=$('#sede').val();
-  if(value==1) $('#foto').html("<img class= 'photos' src='fotos/peru/"+random(codersPeru).image+"'/>")
-  else if (value==2) $('#foto').html("<img class= 'photos' src='fotos/mexico/"+random(codersMexico).image+"'/>")
+  if(value==1) {
+      coder = random(codersPeru)
+      $('#foto').html("<img class= 'photos' src='fotos/peru/"+coder.image+"'/>")}
+  else if (value==2) {
+      coderm = random(codersMexico)
+    $('#foto').html("<img class= 'photos' src='fotos/mexico/"+coderm.image+"'/>")}
 });
+ var cont=0;
+ var puntaje=0;
   $('#comprobar').click(function(){
-    if(($('#inputName').val()==random(codersPeru).nam)||($('#inputName').val()==random(codersMexico).nam)){ $('#mensaje').text("acertaste");}
-    else {$('#mensaje').text("intenta");}
+    if(($('#inputName').val()==coder.nam)||($('#inputName').val()==coderm.nam)){
+        $('#mensaje').text("acertaste");
+        puntaje=puntaje+5;
+        $('#puntaje').html(puntaje);
+        clean();
+        coder = random(codersPeru);
+        coderm = random(codersMexico);
+        $('#foto').html("<img class= 'photos' src='fotos/peru/"+coder.image+"'/>");
+        cont=0;
+    }
+    else {
+        cont++;
+        $('#mensaje').text("intenta nuevamente");
+        puntaje--;
+        $('#puntaje').html(puntaje);
+        clean();
+        if(cont===4) {
+            coder = random(codersPeru);
+            coderm = random(codersMexico);
+            $('#foto').html("<img class= 'photos' src='fotos/peru/"+coder.image+"'/>");
+            cont=0;
+        }
+    }
   });
  });
-
 function random(arrays){
   var longitud=arrays.length;
   var num=Math.round(Math.random() * (longitud)) + 1;
   var imagen=arrays[num];
   return imagen;
+}
+function clean(){
+   var tex = document.getElementById("inputName");
+   tex.value="";
+   tex.focus();
 }
